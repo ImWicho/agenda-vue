eb750e3b8912f6b0cfc68deb4866a4ab5c8046dc
Luis Carrillo <luis.carrilloo.perez@gmail.com>
2021-06-14 18:57:54 -0500

    [ADD] agenda terminada jeje

--- src/components/Agenda.vue
+++ src/components/Agenda.vue
@@ -9,7 +9,7 @@
       <div class="col-xs-12 col-sm-12 col-md-8 col-lg-5">
         <div class="input-group flex-nowrap">
           <span class="input-group-text" id="addon-wrapping"></span>
-          <input type="text" class="form-control" placeholder="Name" />
+          <input type="text" v-model="this.contact.name" class="form-control" placeholder="Name" />
         </div>
       </div>
     </div>
@@ -17,13 +17,24 @@
       <div class="col-xs-12 col-sm-12 col-md-8 col-lg-5">
         <div class="input-group flex-nowrap">
           <span class="input-group-text" id="addon-wrapping"></span>
-          <input type="text" class="form-control" placeholder="Phone" />
+          <input type="text" v-model="this.contact.phone" class="form-control" placeholder="Phone" />
         </div>
       </div>
     </div>
     <div class="row center-xs mt-40">
       <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-        <button type="button" class="btn btn-primary">Add Contact</button>
+        <button type="button" class="btn btn-primary" @click="createContact()">{{ !edit ? 'Add Contact' : 'Finish Edit' }}</button>
+      </div>
+    </div>
+    <div class="row center-xs mt-40">
+      <div class="col-xs-12 col-sm-12 col-md-8 col-lg-5">
+        <ul class="list-group start-xs">
+          <li class="list-group-item row beetwen-xs" v-for="(item, index) in contacts" :key="item">
+            <p>{{ item.name }} - {{ item.phone }}</p>
+            <button type="button" class="btn btn-warning" style="margin-top: 5px;" @click="editContact(index)">Edit</button>
+            <button type="button" class="btn btn-danger" style="margin-top: 5px;" @click="deleteContact(index)">Delete</button>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
@@ -38,13 +49,29 @@ export default {
         phone: '',
       },
       edit: false,
-      contacts: [],
+      contacts: [{ name: 'Luis Carrillo', phone: '8711001907' }],
     };
   },
   methods: {
-    createContact() {},
+    createContact(idx) {
+      const newContact = { ...this.contact };
+      
+      if(!this.edit){
+        this.contacts.push(newContact);
+      }else{
+        this.contacts[idx] = newContact;
+        this.edit = false;
+      }
+
+      this.contact = { name: '', phone: '' };
+    },
     editContact(idx) {
       this.contact = this.contacts[idx];
+      this.edit = true;
+    },
+    deleteContact(idx) {
+      this.contacts.splice(idx, 1);
+      this.contact = { name: '', phone: '' };
     },
   },
 };
