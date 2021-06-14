19adbf72aeb4bd79f61d43ce82274ee0880632b7
Luis Carrillo <luis.carrilloo.perez@gmail.com>
2021-06-14 18:27:48 -0500

    [ADD] desing was added

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Agenda/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Agenda from './components/Agenda.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Agenda
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "styles.css";
 </style>
---
+++ src/components/Agenda.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container agenda">
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 center-xs">
+        <h1>Agenda</h1>
+      </div>
+    </div>
+    <div class="row center-xs mt-40">
+      <div class="col-xs-12 col-sm-12 col-md-8 col-lg-5">
+        <div class="input-group flex-nowrap">
+          <span class="input-group-text" id="addon-wrapping"></span>
+          <input type="text" class="form-control" placeholder="Name" />
+        </div>
+      </div>
+    </div>
+    <div class="row center-xs mt-40">
+      <div class="col-xs-12 col-sm-12 col-md-8 col-lg-5">
+        <div class="input-group flex-nowrap">
+          <span class="input-group-text" id="addon-wrapping"></span>
+          <input type="text" class="form-control" placeholder="Phone" />
+        </div>
+      </div>
+    </div>
+    <div class="row center-xs mt-40">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <button type="button" class="btn btn-primary">Add Contact</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Agenda',
+  data() {
+    return {
+      contact: {
+        name: '',
+        phone: '',
+      },
+      edit: false,
+      contacts: [],
+    };
+  },
+  methods: {
+    createContact() {},
+    editContact(idx) {
+      this.contact = this.contacts[idx];
+    },
+  },
+};
+</script>
+<style></style>
